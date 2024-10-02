19a4f44ba9a00684356a7b527be5770ca92ea778
shanthimsd <madasushanthi7@gmail.com>
2024-10-02 16:19:35 +0800

    user profile

---
+++ outputs.tf
@@ -0,0 +1,34 @@
+output "vpc_id" {
+    value = aws_vpc.main.id
+  
+}
+
+output "az_info" {
+    value = data.aws_availability_zones.available
+  
+} 
+
+output "default_vpc_info" {
+    value = data.aws_vpc.default
+  
+}
+
+output "main_route_table_info" {
+   value = data.aws_route_table.main
+ }
+
+ output "public_subnet_ids"{
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids"{
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids"{
+  value = aws_subnet.database[*].id
+}
+
+# output "database_subnet_group_name"{
+#   value = aws_database_subnet_group.default.name
+# }
